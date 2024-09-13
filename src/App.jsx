d74a6d466dedf754e6518455357e8b539905156b
ID22663 <gayratyoldoshboyev3@gmail.com>
2024-09-13 18:40:29 +0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./index.css";
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import LineChart from "./LineChart";
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <LineChart />
+    </div>
+  );
+}
